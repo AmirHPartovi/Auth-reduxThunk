9b6f9f27a5d13097d57a00915f345ff5b52f4988
Amir partovi <a.partovi99@gmail.com>
2022-10-24 19:05:05 +0330

    create extraReducer

--- src/slice/AuthSlice.js
+++ src/slice/AuthSlice.js
@@ -1,9 +1,13 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { Login } from "../api/login";
 
 export const AuthAsyncThunk = createAsyncThunk(
     'auth/Login',
     //authData => {userName:'amir' , passWord:'123456}
-    ()=>{}
+    async(authData)=>{
+        const data = await Login(authData.userName , authData.passWord);
+        return data
+    }//1:Pendding 2:fullfilled 3:rejected
 )
 
 const initialState ={
@@ -14,8 +18,16 @@ export const AuthSlice = createSlice({
     name:'Auth',
     initialState,
     reducers:{},
-    extraReducers:()=>{
+    extraReducers:(builder)=>{
+        builder.addCase(AuthAsyncThunk.pending,()=>{
+            
+        })
+        builder.addCase(AuthAsyncThunk.fulfilled,()=>{
 
+        })
+        builder.addCase(AuthAsyncThunk.rejected,()=>{
+
+        })
     }
 })
 
