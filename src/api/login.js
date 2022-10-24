4163afd68402f083d8cda26a340d5daec21a6e12
Amir partovi <a.partovi99@gmail.com>
2022-10-24 19:54:54 +0330

    add setTimeOut

--- src/api/login.js
+++ src/api/login.js
@@ -1,11 +1,14 @@
 export const Login = (userName , passWord)=>{
     return new Promise((resolve,reject)=>{
         if (userName==='amir' && passWord==='123456'){
-            resolve({
-                name:'amir',
-                age:24,
-                country:'iran'
-            })
+            setTimeout(()=>{
+                resolve({
+                    name:'amir',
+                    age:24,
+                    country:'iran'
+                })
+            },5000)
+            
         }else{
             reject('invalid Username or Password')
         }
